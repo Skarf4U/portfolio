469ab3cab3ca3f97a9563fff2984a24129be8521
Skarf <54850873+Skarf4U@users.noreply.github.com>
2023-10-13 00:16:41 +0300

    theme.js testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,25 @@
-import { useAnalytics } from "@/lib/useAnalytics"
-import { ThemeProvider } from "next-themes"
-import type { AppProps } from "next/app"
-import Head from "next/head"
-import "../styles/globals.css"
-import "../styles/projects.css"
-import "../styles/about.css"
-import "../styles/skills.css"
+import { useAnalytics } from "@/lib/useAnalytics";
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
+import "../styles/globals.css";
+import "../styles/projects.css";
+import "../styles/about.css";
+import "../styles/skills.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useAnalytics()
+  useAnalytics();
 
   return (
     <>
       <Head>
         <title>Adi Amar</title>
       </Head>
+      <Script src="/theme.js" strategy="beforeInteractive" />
       <ThemeProvider attribute="class" enableColorScheme={false}>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
