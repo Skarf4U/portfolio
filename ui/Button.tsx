e1c11bc54a7fe63a718c09a31c4a2523b99c5350
Skarf <54850873+Skarf4U@users.noreply.github.com>
2023-10-13 00:05:48 +0300

    btn fix

--- ui/Button.tsx
+++ ui/Button.tsx
@@ -1,33 +1,39 @@
-import Link from "next/link"
-import cx from "clsx"
+import Link from "next/link";
+import cx from "clsx";
+import { ReactNode } from "react";
 
-export const Button = ({
+type ButtonProps = {
+  children: ReactNode;
+  href: string;
+  color?: "primary" | "secondary";
+} & Omit<React.HTMLProps<HTMLAnchorElement>, "href">;
+
+const Button = ({
   children,
   href,
   color = "primary",
   ...props
-}: {
-  href: string
-  color?: "primary" | "secondary"
-} & Omit<React.HTMLProps<HTMLAnchorElement>, "href">) => {
+}: ButtonProps) => {
   return (
     <div>
-      <Link
-        href={href}
-        className={cx(
-          "block px-8 py-2 text-center transition duration-200 border rounded focus:outline-none",
-          {
-            "text-white bg-gray-900  border-transparent  hover:bg-white hover:text-gray-800 hover:border-gray-800":
-              color === "primary",
-            "text-gray-600 border-gray-300 hover:border-gray-800 hover:text-gray-800":
-              color === "secondary",
-          },
-        )}
-        {...props}>
-
-        {children}
-
+      <Link href={href}>
+        <a
+          className={cx(
+            "block px-8 py-2 text-center transition duration-200 border rounded focus:outline-none",
+            {
+              "text-white bg-gray-900 border-transparent hover:bg-white hover:text-gray-800 hover:border-gray-800":
+                color === "primary",
+              "text-gray-600 border-gray-300 hover:border-gray-800 hover:text-gray-800":
+                color === "secondary",
+            }
+          )}
+          {...props}
+        >
+          {children}
+        </a>
       </Link>
     </div>
   );
-}
+};
+
+export default Button;
