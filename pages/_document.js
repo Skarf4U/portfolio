db132d2e09e2aedd7215633b9b745ada30681f13
Skarf <54850873+Skarf4U@users.noreply.github.com>
2023-10-13 00:09:25 +0300

    light mode

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document"
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html className="light">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
